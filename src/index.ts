f8424e9c8b92ed2bf2dfe92911cc10c9ed586848
dlduddnd <lyw051015@naver.com>
2024-09-24 21:09:22 +0900

    프로젝트 초기화 완료

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+console.log('Hello World');
+
+let x = 0;
+let y = true;
+let z = "hello";
+let w = undefined;
+let v = null;
+
+let object = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
+let list1 = [1,2,3,4,5];
+let list2 = [1, "hello", 3, "world", 5];
+let list3 = [];
